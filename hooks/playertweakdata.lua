6b481620b34b3e5b92726588fec97dccf02fad87
IKillerwolfI <killerwolf.no1@gmail.com>
2018-11-02 23:11:52 +0100

    Sight alignment
    ******

--- hooks/playertweakdata.lua
+++ hooks/playertweakdata.lua
@@ -3,10 +3,6 @@ Hooks:PostHook( PlayerTweakData, "init", "AK47ModStanceTweakData", function(self
 
 --AK
 	
-	self.stances.heffy_762_ads = deep_clone(self.stances.flint)
-	local ironsight_translation = Vector3(0, -13, 0.518)
-	self.stances.heffy_762_ads.steelsight.shoulders.translation = self.stances.heffy_762_ads.steelsight.shoulders.translation + ironsight_translation 
-
 	self.stances.akmsu_ads = deep_clone(self.stances.flint)
 	local ironsight_translation = Vector3(0, -13, -0.4)
 	self.stances.akmsu_ads.steelsight.shoulders.translation = self.stances.akmsu_ads.steelsight.shoulders.translation + ironsight_translation
